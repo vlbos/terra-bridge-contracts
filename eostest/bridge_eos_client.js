01b4d391929b0a2262d9e5093fbc27f2bf8e3bfc
vlbos <vlbos2018@gmail.com>
2020-01-19 11:33:40 +0800

    foreign ok

--- eostest/bridge_eos_client.js
+++ eostest/bridge_eos_client.js
@@ -23,7 +23,7 @@ const oracle = "oracleoracle";
 
 const eos = Eos({
 	httpEndpoint: process.env.EOS_PROTOCOL + "://" + process.env.EOS_HOST + ":" + process.env.EOS_PORT,
-	keyProvider: [process.env.EOS_KEY, '5J1G4dhajiWDQduM3WSJ26vuoaMHi1AoqFLgVpazHL2aHsMkSb1','5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3','5JhNVeWb8DnMwczC54PSeGBYeQgjvW4SJhVWXMXW7o4f3xh7sYk', '5JBqSZmzhvf3wopwyAwXH5g2DuNw9xdwgnTtuWLpkWP7YLtDdhp', '5JCtWxuqPzcPUfFukj58q8TqyRJ7asGnhSYvvxi16yq3c5p6JRG', '5K79wAY8rgPwWQSRmyQa2BR8vPicieJdLCXL3cM5Db77QnsJess', "5K2L2my3qUKqj67KU61cSACoxgREkqGFi5nKaLGjbAbbRBYRq1m", "5JN8chYis1d8EYsCdDEKXyjLT3QmpW7HYoVB13dFKenK2uwyR65", "5Kju7hDTh3uCZqpzb5VWAdCp7cA1fAiEd94zdNhU59WNaQMQQmE", "5K6ZCUpk2jn1munFdiADgKgfAqcpGMHKCoJUue65p99xKX9WWCW", "5KAyefwicvJyxDaQ1riCztiSgVKiH37VV9JdSRcrqi88qQkV2gJ"],
+	keyProvider: [process.env.EOS_KEY, '5J1G4dhajiWDQduM3WSJ26vuoaMHi1AoqFLgVpazHL2aHsMkSb1', '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3', '5JhNVeWb8DnMwczC54PSeGBYeQgjvW4SJhVWXMXW7o4f3xh7sYk', '5JBqSZmzhvf3wopwyAwXH5g2DuNw9xdwgnTtuWLpkWP7YLtDdhp', '5JCtWxuqPzcPUfFukj58q8TqyRJ7asGnhSYvvxi16yq3c5p6JRG', '5K79wAY8rgPwWQSRmyQa2BR8vPicieJdLCXL3cM5Db77QnsJess', "5K2L2my3qUKqj67KU61cSACoxgREkqGFi5nKaLGjbAbbRBYRq1m", "5JN8chYis1d8EYsCdDEKXyjLT3QmpW7HYoVB13dFKenK2uwyR65", "5Kju7hDTh3uCZqpzb5VWAdCp7cA1fAiEd94zdNhU59WNaQMQQmE", "5K6ZCUpk2jn1munFdiADgKgfAqcpGMHKCoJUue65p99xKX9WWCW", "5KAyefwicvJyxDaQ1riCztiSgVKiH37VV9JdSRcrqi88qQkV2gJ"],
 	chainId: process.env.EOS_CHAIN,
 	verbose: false,
 	logger: {
@@ -121,74 +121,71 @@ const request_id = 0;
 
 
 
-function transferfromhome_sig(contract)
-{
+function transferfromhome_sig(contract) {
 
 	// std::string token;
 	// name recipient;
 	// uint64_t amount;
 	// checksum256 txHash;
-	const v = 1000;
-	const vv="ss";
-				 const message = {
-							 token: v,
-							 rec: v,
-							 amount:v,
-							 txhash:vv
-						 };
-						 const messageBinary = contract.fc.toBuffer("msgdatas", message);
-						//  let a = ecc.sha256(JSON.stringify("Hello)); 
-						//  ecc.sha256(Buffer.from(a, 'hex'))
-	let pvts = 	["5K2L2my3qUKqj67KU61cSACoxgREkqGFi5nKaLGjbAbbRBYRq1m","5JN8chYis1d8EYsCdDEKXyjLT3QmpW7HYoVB13dFKenK2uwyR65", "5Kju7hDTh3uCZqpzb5VWAdCp7cA1fAiEd94zdNhU59WNaQMQQmE","5K6ZCUpk2jn1munFdiADgKgfAqcpGMHKCoJUue65p99xKX9WWCW","5KAyefwicvJyxDaQ1riCztiSgVKiH37VV9JdSRcrqi88qQkV2gJ"];
-	  
-const wif = '5J5twkfSgL3SgWNKDsD5bjvevdmbXD5faBGcybJVAmYjCJXvpbJ'
-let sigs =[];
-					for(let j=0;j<5;j++)
-					{
-						sigs.push(ecc.sign(messageBinary, pvts[j]));// String.fromCharCode(str1.charCodeAt()+i)
-					}
-					return {sigs:sigs,message:messageBinary};
-// const sig = ecc.sign(message, wif)
 
-// console.log('Public Key:', ecc.privateToPublic(wif)) // EOS68vRVaNgCvStaUmxQsKoHANU1Uypo4BQLWSNEM8KBiCAWW8deh
+	const message = {
+		token: "eosio.token:BOS:4",
+		recipient: "consumer2222",
+		amount: 10000,
+		txhash: ecc.sha256("11")
+	};
+	const messageBinary = contract.fc.toBuffer("message_data", message);
+	//  let a = ecc.sha256(JSON.stringify("Hello)); 
+	//  ecc.sha256(Buffer.from(a, 'hex'))
+	let pvts = ["5K2L2my3qUKqj67KU61cSACoxgREkqGFi5nKaLGjbAbbRBYRq1m", "5JN8chYis1d8EYsCdDEKXyjLT3QmpW7HYoVB13dFKenK2uwyR65", "5Kju7hDTh3uCZqpzb5VWAdCp7cA1fAiEd94zdNhU59WNaQMQQmE", "5K6ZCUpk2jn1munFdiADgKgfAqcpGMHKCoJUue65p99xKX9WWCW", "5KAyefwicvJyxDaQ1riCztiSgVKiH37VV9JdSRcrqi88qQkV2gJ"];
 
-// console.log('Signature:', sig) // SIG_K1_KcB1jGNsjYEE7Gby6X7KZ9z6BFVfHPey6DUayYtDagXsbzr4Tbfpq5TS2JvYzs3oMg9QGAugTyGXoTVe7DujeXpDX5KYfJ
+	const wif = '5J5twkfSgL3SgWNKDsD5bjvevdmbXD5faBGcybJVAmYjCJXvpbJ'
+	let sigs = [];
+	for (let j = 0; j < 5; j++) {
+		sigs.push(ecc.sign(messageBinary, pvts[j]));// String.fromCharCode(str1.charCodeAt()+i)
+	}
+	return { sigs: sigs, message: messageBinary };
+	// const sig = ecc.sign(message, wif)
+
+	// console.log('Public Key:', ecc.privateToPublic(wif)) // EOS68vRVaNgCvStaUmxQsKoHANU1Uypo4BQLWSNEM8KBiCAWW8deh
+
+	// console.log('Signature:', sig) // SIG_K1_KcB1jGNsjYEE7Gby6X7KZ9z6BFVfHPey6DUayYtDagXsbzr4Tbfpq5TS2JvYzs3oMg9QGAugTyGXoTVe7DujeXpDX5KYfJ
 }
 
 
-function test_sig(contract)
-{
+function test_sig(contract) {
 
 	// std::string token;
 	// name recipient;
 	// uint64_t amount;
 	// checksum256 txHash;
 
-				 const message = {
-							 token: "eosio.token:BOS:4",
-							 recipient: "consumer2222",
-							 amount:10000,
-							 txHash:ecc.sha256("11")
-						 };
-						 const messageBinary = contract.fc.toBuffer("message_data", message);
-						//  let a = ecc.sha256(JSON.stringify("Hello)); 
-						//  ecc.sha256(Buffer.from(a, 'hex'))
-	let pvts = 	["5K2L2my3qUKqj67KU61cSACoxgREkqGFi5nKaLGjbAbbRBYRq1m","5JN8chYis1d8EYsCdDEKXyjLT3QmpW7HYoVB13dFKenK2uwyR65", "5Kju7hDTh3uCZqpzb5VWAdCp7cA1fAiEd94zdNhU59WNaQMQQmE","5K6ZCUpk2jn1munFdiADgKgfAqcpGMHKCoJUue65p99xKX9WWCW","5KAyefwicvJyxDaQ1riCztiSgVKiH37VV9JdSRcrqi88qQkV2gJ"];
-	  
-const wif = '5K2L2my3qUKqj67KU61cSACoxgREkqGFi5nKaLGjbAbbRBYRq1m'
-// let sigs =[];
-// 					for(let j=0;j<5;j++)
-// 					{
-// 						sigs.push(ecc.sign(messageBinary, pvts[j]));// String.fromCharCode(str1.charCodeAt()+i)
-// 					}
-// 					return {sigs:sigs,message:messageBinary};
-const sig = ecc.sign(messageBinary, wif)
-const pk =ecc.privateToPublic(wif);
-console.log('Public Key:',pk ) // EOS68vRVaNgCvStaUmxQsKoHANU1Uypo4BQLWSNEM8KBiCAWW8deh
+	const message = {
+		token: "eosio.token:BOS:4",
+		recipient: "consumer2222",
+		amount: 10000,
+		txhash: ecc.sha256("11")
+	};
+	const messageBinary = contract.fc.toBuffer("message_data", message);
 
-console.log('Signature:', sig) // SIG_K1_KcB1jGNsjYEE7Gby6X7KZ9z6BFVfHPey6DUayYtDagXsbzr4Tbfpq5TS2JvYzs3oMg9QGAugTyGXoTVe7DujeXpDX5KYfJ
+	//  let a = ecc.sha256(JSON.stringify("Hello)); 
+	//  ecc.sha256(Buffer.from(a, 'hex'))
+	let pvts = ["5K2L2my3qUKqj67KU61cSACoxgREkqGFi5nKaLGjbAbbRBYRq1m", "5JN8chYis1d8EYsCdDEKXyjLT3QmpW7HYoVB13dFKenK2uwyR65", "5Kju7hDTh3uCZqpzb5VWAdCp7cA1fAiEd94zdNhU59WNaQMQQmE", "5K6ZCUpk2jn1munFdiADgKgfAqcpGMHKCoJUue65p99xKX9WWCW", "5KAyefwicvJyxDaQ1riCztiSgVKiH37VV9JdSRcrqi88qQkV2gJ"];
 
-return {pk:pk,sig:sig,message:messageBinary};
+	const wif = '5K2L2my3qUKqj67KU61cSACoxgREkqGFi5nKaLGjbAbbRBYRq1m'
+	// let sigs =[];
+	// 					for(let j=0;j<5;j++)
+	// 					{
+	// 						sigs.push(ecc.sign(messageBinary, pvts[j]));// String.fromCharCode(str1.charCodeAt()+i)
+	// 					}
+	// 					return {sigs:sigs,message:messageBinary};
+	const sig = ecc.sign(messageBinary, wif)
+	const pk = ecc.privateToPublic(wif);
+	console.log('Public Key:', pk) // EOS68vRVaNgCvStaUmxQsKoHANU1Uypo4BQLWSNEM8KBiCAWW8deh
+
+	console.log('Signature:', sig) // SIG_K1_KcB1jGNsjYEE7Gby6X7KZ9z6BFVfHPey6DUayYtDagXsbzr4Tbfpq5TS2JvYzs3oMg9QGAugTyGXoTVe7DujeXpDX5KYfJ
+
+	return { pk: pk, sig: sig, message: messageBinary };
 }
 
 
@@ -199,7 +196,7 @@ return {pk:pk,sig:sig,message:messageBinary};
 // const update_start_time = "2019-09-16 09:09:09";
 class BridgeEosClient {
 	constructor() {
-		
+
 	}
 
 	test() {
@@ -210,14 +207,14 @@ class BridgeEosClient {
 				console.log(obj.sig);
 				console.log({
 					sender: consumer,
-					sender_key:obj.pk, 
-					sig:obj.sig,
+					sender_key: obj.pk,
+					sig: obj.sig,
 					message: obj.message
 				})
 				contract.test({
 					sender: consumer,
-					sender_key:obj.pk, 
-					sig:obj.sig,
+					sender_key: obj.pk,
+					sig: obj.sig,
 					message: obj.message
 				},
 					{
@@ -240,13 +237,13 @@ class BridgeEosClient {
 	setparameter() {
 		console.log("results:", "results");
 		eos.contract(oracleContract)
-		.then((contract) => {
+			.then((contract) => {
 				contract.setparameter({
 					version: 1,
 					core_symbol: "BOS",
 					precision: 4,
-					foreign:{validatorContractAddress:"burn.bos",gasPrice:100,requiredBlockConfirmations:1,minPerTx:1,maxPerTx:100000,dailyLimit:1000000},
-					home:{validatorContractAddress:"burn.bos",gasPrice:100,requiredBlockConfirmations:1,minPerTx:1,maxPerTx:100000,dailyLimit:1000000}
+					foreign: { validatorContractAddress: "burn.bos", gasPrice: 100, requiredBlockConfirmations: 1, minPerTx: 1, maxPerTx: 100000, dailyLimit: 1000000 },
+					home: { validatorContractAddress: "burn.bos", gasPrice: 100, requiredBlockConfirmations: 1, minPerTx: 1, maxPerTx: 100000, dailyLimit: 1000000 }
 				},
 					{
 						scope: oracleContract,
@@ -260,42 +257,42 @@ class BridgeEosClient {
 					});
 
 
-		})
-		.catch(error => {
-			console.log("error:", error);
-		});
-}
+			})
+			.catch(error => {
+				console.log("error:", error);
+			});
+	}
 
-impvalidator() {
-			eos.contract(oracleContract)
+	impvalidator() {
+		eos.contract(oracleContract)
 			.then((contract) => {
 				// sleep.sleep(2);
 				// var str1 = 'a';
-					// let i = 1;
-					let pks=["EOS6U2CbfrXa9hdKauZJxxbmoXACZ4MmAWHKaQPzCk5UiBmVhZRTJ", "EOS7qsja8UCa1ExokEb5wxCwBmJWi9aW1intH1sihNNHKoAGD6J7X", "EOS7yghCVnJHEu3TEB2nnSv1mgS5Rx8ofDyQK7C4dgbUWZCP1TtD1","EOS6jmPJZAPAB7hBwYxwfKiwVuqSrkSyRy2E4mjTmQ2CyYas4ESuv" ,"EOS8hvj4KPjjGvfRfJsGEEbVvCXvAiGQ7GW345MH1r122g8Ap7xw3"];
-					let providers =[];
-					for(let j=0;j<pks.length;j++)
+				// let i = 1;
+				let pks = ["EOS6U2CbfrXa9hdKauZJxxbmoXACZ4MmAWHKaQPzCk5UiBmVhZRTJ", "EOS7qsja8UCa1ExokEb5wxCwBmJWi9aW1intH1sihNNHKoAGD6J7X", "EOS7yghCVnJHEu3TEB2nnSv1mgS5Rx8ofDyQK7C4dgbUWZCP1TtD1", "EOS6jmPJZAPAB7hBwYxwfKiwVuqSrkSyRy2E4mjTmQ2CyYas4ESuv", "EOS8hvj4KPjjGvfRfJsGEEbVvCXvAiGQ7GW345MH1r122g8Ap7xw3"];
+				let providers = [];
+				for (let j = 0; j < pks.length; j++) {
+					let n = "provider" + repeat(j + 1, 4);
+					let k = pks[j];
+					let o = { "first": n, "second": k };
+					providers.push(o);// String.fromCharCode(str1.charCodeAt()+i)
+				}
+				console.log(providers);
+				contract.impvalidator({
+					requiredSignatures: 5,
+					initialValidators: providers,
+					owner: "burn.bos"
+				},
 					{
-						let n = "provider" +repeat(j+1,4);0
-						let k = pks[j];
-						let o = {"first":n,"second":k};
-						providers.push(o);// String.fromCharCode(str1.charCodeAt()+i)
-					}
-				     console.log(providers);
-					contract.impvalidator({requiredSignatures:5,
-						initialValidators:providers,
-						owner:"burn.bos"
-					},
-						{
-							scope: oracleContract,
-							authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
-						})
-						.then(results => {
-							console.log("results:", results);
-						})
-						.catch(error => {
-							console.log("error:", error);
-						});
+						scope: oracleContract,
+						authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
+					})
+					.then(results => {
+						console.log("results:", results);
+					})
+					.catch(error => {
+						console.log("error:", error);
+					});
 			})
 			.catch(error => {
 				console.log("error:", error);
@@ -304,10 +301,10 @@ impvalidator() {
 
 	transferNativeToHome() {
 
-		 const pub = "EOS5NkC58kuahypYnbyYXEZvwau1KbD1rmRDJD2R61CzKaznnWH3y";
- eos.transaction(allowContract(consumer, pub, oracleContract));
+		const pub = "EOS5NkC58kuahypYnbyYXEZvwau1KbD1rmRDJD2R61CzKaznnWH3y";
+		eos.transaction(allowContract(consumer, pub, oracleContract));
 
-  eos.contract(oracleContract)
+		eos.contract(oracleContract)
 			.then((contract) => {
 				contract.transfern2h({
 					sender: "consumer1111",
@@ -335,7 +332,7 @@ impvalidator() {
 			.then((contract) => {
 				contract.transfert2h({
 					sender: "consumer1111",
-					token:"eosio.token:BOSS:4",
+					token: "eosio.token:BOSS:4",
 					recipient: "consumer2222",
 					value: 10000
 				},
@@ -355,14 +352,14 @@ impvalidator() {
 			});
 	}
 
-	
+
 	transferFromHome() {
 		eos.contract(oracleContract)
 			.then((contract) => {
 				let obj = transferfromhome_sig(contract);
 				contract.transferfrom({
-					sender: consumer, 
-					sig:obj.sigs,
+					sender: consumer,
+					sig: obj.sigs,
 					message: obj.message
 				},
 					{
@@ -380,147 +377,145 @@ impvalidator() {
 				console.log("error:", error);
 			});
 	}
-////hometoken
+	////hometoken
 
-registerToken() {
-	eos.contract(oracleContract)
-		.then((contract) => {
-			contract.regtoken({
-				sender: "consumer1111",
-				foreignAddress:"ETH",
-				homeAddress: "eosio.token:ETHT:4"
-			},
-				{
-					scope: oracleContract,
-					authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
-				})
-				.then(results => {
-					console.log("results:", results);
-				})
-				.catch(error => {
-					console.log("error:", error);
-				});
-		})
-		.catch(error => {
-			console.log("error:", error);
-		});
-}
+	registerToken() {
+		eos.contract(oracleContract)
+			.then((contract) => {
+				contract.regtoken({
+					sender: "consumer1111",
+					foreignAddress: "ETH",
+					homeAddress: "eosio.token:ETHT:4"
+				},
+					{
+						scope: oracleContract,
+						authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
+					})
+					.then(results => {
+						console.log("results:", results);
+					})
+					.catch(error => {
+						console.log("error:", error);
+					});
+			})
+			.catch(error => {
+				console.log("error:", error);
+			});
+	}
 
-transferNativeToForeign() {
-	eos.contract(oracleContract)
-		.then((contract) => {
-			contract.transfern2f({
-				sender: "consumer1111",
-				recipient: "consumer2222",
-				value: 10000
-			},
-				{
-					scope: oracleContract,
-					authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
-				})
-				.then(results => {
-					console.log("results:", results);
-				})
-				.catch(error => {
-					console.log("error:", error);
-				});
-		})
-		.catch(error => {
-			console.log("error:", error);
-		});
-}
+	transferNativeToForeign() {
+		eos.contract(oracleContract)
+			.then((contract) => {
+				contract.transfern2f({
+					sender: "consumer1111",
+					recipient: "consumer2222",
+					value: 10000
+				},
+					{
+						scope: oracleContract,
+						authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
+					})
+					.then(results => {
+						console.log("results:", results);
+					})
+					.catch(error => {
+						console.log("error:", error);
+					});
+			})
+			.catch(error => {
+				console.log("error:", error);
+			});
+	}
 
-transferTokenToForeign() {
-	eos.contract(oracleContract)
-		.then((contract) => {
-			contract.transfert2f({
-				sender: "consumer1111",
-				token:"BOSS",
-				recipient: "consumer2222",
-				value: 10000
-			},
-				{
-					scope: oracleContract,
-					authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
-				})
-				.then(results => {
-					console.log("results:", results);
-				})
-				.catch(error => {
-					console.log("error:", error);
-				});
-		})
-		.catch(error => {
-			console.log("error:", error);
-		});
-}
+	transferTokenToForeign() {
+		eos.contract(oracleContract)
+			.then((contract) => {
+				contract.transfert2f({
+					sender: "consumer1111",
+					token: "BOSS",
+					recipient: "consumer2222",
+					value: 10000
+				},
+					{
+						scope: oracleContract,
+						authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
+					})
+					.then(results => {
+						console.log("results:", results);
+					})
+					.catch(error => {
+						console.log("error:", error);
+					});
+			})
+			.catch(error => {
+				console.log("error:", error);
+			});
+	}
 
-transferFromForeign() {
-	eos.contract(oracleContract)
-		.then((contract) => {
-				let sigs =[];
-				for(let j=1;j<=5;j++)
-				{
-					sigs.push("provider" +repeat(j,4));// String.fromCharCode(str1.charCodeAt()+i)
+	transferFromForeign() {
+		eos.contract(oracleContract)
+			.then((contract) => {
+				let sigs = [];
+				for (let j = 1; j <= 5; j++) {
+					sigs.push("provider" + repeat(j, 4));// String.fromCharCode(str1.charCodeAt()+i)
 				}
-					
-			contract.transferfrof({
-				sender: "consumer1111",
-				foreignToken:"ETH",
-				recipient: "consumer2222",
-				value: 10000,
-				transactionHash: "consumer2222"
-			},
-				{
-					scope: oracleContract,
-					authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
-				})
-				.then(results => {
-					console.log("results:", results);
-				})
-				.catch(error => {
-					console.log("error:", error);
-				});
-		})
-		.catch(error => {
-			console.log("error:", error);
-		});
-}
 
-submitSignature() {
-	eos.contract(oracleContract)
-		.then((contract) => {
-				let sigs =[];
-				for(let j=1;j<=5;j++)
-				{
-					sigs.push("provider" +repeat(j,4));// String.fromCharCode(str1.charCodeAt()+i)
+				contract.transferfrof({
+					sender: "consumer1111",
+					foreignToken: "ETH",
+					recipient: "consumer2222",
+					value: 10000,
+					transactionHash: "consumer2222"
+				},
+					{
+						scope: oracleContract,
+						authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
+					})
+					.then(results => {
+						console.log("results:", results);
+					})
+					.catch(error => {
+						console.log("error:", error);
+					});
+			})
+			.catch(error => {
+				console.log("error:", error);
+			});
+	}
+
+	submitSignature() {
+		eos.contract(oracleContract)
+			.then((contract) => {
+				let sigs = [];
+				for (let j = 1; j <= 5; j++) {
+					sigs.push("provider" + repeat(j, 4));// String.fromCharCode(str1.charCodeAt()+i)
 				}
 				// const price = {
-						// 	value: 200000,
-						// 	decimals: 4
-						// };
-						// const priceBinary = contract.fc.toBuffer("price", price);
-			contract.submitsig({
-				sender: "consumer1111",
-				sender_key:"",
-				sig:sigs,
-				message: "consumer2222"
-			},
-				{
-					scope: oracleContract,
-					authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
-				})
-				.then(results => {
-					console.log("results:", results);
-				})
-				.catch(error => {
-					console.log("error:", error);
-				});
-		})
-		.catch(error => {
-			console.log("error:", error);
-		});
-}
+				// 	value: 200000,
+				// 	decimals: 4
+				// };
+				// const priceBinary = contract.fc.toBuffer("price", price);
+				contract.submitsig({
+					sender: "consumer1111",
+					sender_key: "",
+					sig: sigs,
+					message: "consumer2222"
+				},
+					{
+						scope: oracleContract,
+						authorization: [`${oracleContract}@${process.env.ORACLE_PERMISSION || 'active'}`]
+					})
+					.then(results => {
+						console.log("results:", results);
+					})
+					.catch(error => {
+						console.log("error:", error);
+					});
+			})
+			.catch(error => {
+				console.log("error:", error);
+			});
+	}
 
 }
 
